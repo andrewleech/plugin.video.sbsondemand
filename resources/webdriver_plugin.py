978fe0423329c89486e3a1a055c0d94eae014389
Andrew Leech <andrew@alelec.net>
2015-09-07 21:39:14 +1000

    Use correct key mapping from web driver plugin

--- resources/webdriver_plugin.py
+++ resources/webdriver_plugin.py
@@ -2,6 +2,7 @@
 import os
 import json
 from webdriver import Browser, Keys, By, js_fn
+from webdriver._utils import *
 PLUGIN_NAME="sbs_ondemand"
 
 jsTargetBy = By.ID
@@ -9,14 +10,14 @@ jsTarget = 'player'
 
 import xbmcgui
 SBS_KEYMAP = {
-    xbmcgui.ACTION_SELECT_ITEM   : ("handle_key",js_fn.keynameToKeycode.Space),
-    xbmcgui.ACTION_MOVE_RIGHT    : ("handle_key",js_fn.keynameToKeycode.Right),
-    xbmcgui.ACTION_MOVE_LEFT     : ("handle_key",js_fn.keynameToKeycode.Left),
+    xbmcgui.ACTION_SELECT_ITEM   : ("handle_key",js_fn.keynameToKeycode.space),
+    xbmcgui.ACTION_MOVE_RIGHT    : ("handle_key",js_fn.keynameToKeycode.right),
+    xbmcgui.ACTION_MOVE_LEFT     : ("handle_key",js_fn.keynameToKeycode.left),
     xbmcgui.ACTION_MOVE_UP       : ("skip_forward",None),
     xbmcgui.ACTION_MOVE_DOWN     : ("skip_back",None),
-    xbmcgui.ACTION_VOLUME_UP     : ("handle_key",js_fn.keynameToKeycode.Up),
-    xbmcgui.ACTION_VOLUME_DOWN   : ("handle_key",js_fn.keynameToKeycode.Down),
-    xbmcgui.ACTION_PLAY          : ("handle_key",js_fn.keynameToKeycode.Space),
+    xbmcgui.ACTION_VOLUME_UP     : ("handle_key",js_fn.keynameToKeycode.up),
+    xbmcgui.ACTION_VOLUME_DOWN   : ("handle_key",js_fn.keynameToKeycode.down),
+    xbmcgui.ACTION_PLAY          : ("handle_key",js_fn.keynameToKeycode.space),
     xbmcgui.ACTION_NAV_BACK      : ("exit",None),
     xbmcgui.ACTION_PARENT_DIR    : ("exit",None),
     xbmcgui.ACTION_PREVIOUS_MENU : ("exit",None),
@@ -36,7 +37,6 @@ class PLUGIN(object):
     def action_handle_key(self, keyCode):
         js_key = {'keyCode':keyCode}
         ret = self.chrome.execute_script('window.embeddedPlayer.plugins.sbsmisc.handleKeyboardInput(%s);' % str(js_key) )
-        # log("keyCode:%d , res: %s"%(keyCode, str(ret)))
 
     def action_skip_forward(self, *_):
         # sbs keyboard controls don't include larger skips, so go straight to js
